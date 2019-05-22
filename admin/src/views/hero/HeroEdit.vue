76eb60c8f240eb233a257f4c8b039b18338ee3a2
lwq945 <lwq945@icloud.com>
2019-05-22 18:30:48 +0800

    完成英雄模型字段在页面对应的内容编辑

--- admin/src/views/hero/HeroEdit.vue
+++ admin/src/views/hero/HeroEdit.vue
@@ -16,6 +16,50 @@
           <i v-else class="el-icon-plus avatar-uploader-icon"></i>
         </el-upload>
       </el-form-item>
+      <el-form-item label="称号">
+        <el-input v-model="model.title"></el-input>
+      </el-form-item>
+      <el-form-item label="类型">
+        <el-select v-model="model.categories" multiple>
+          <el-option
+            v-for="item of categories"
+            :key="item._id"
+            :label="item.name"
+            :value="item._id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="难度">
+        <el-rate v-model="model.scores.difficult" :max="10" show-score style="margin-top:12px"></el-rate>
+      </el-form-item>
+      <el-form-item label="技能">
+        <el-rate v-model="model.scores.skills" :max="10" show-score style="margin-top:12px"></el-rate>
+      </el-form-item>
+      <el-form-item label="攻击">
+        <el-rate v-model="model.scores.attack" :max="10" show-score style="margin-top:12px"></el-rate>
+      </el-form-item>
+      <el-form-item label="生存">
+        <el-rate v-model="model.scores.survive" :max="10" show-score style="margin-top:12px"></el-rate>
+      </el-form-item>
+      <el-form-item label="顺风出装">
+        <el-select v-model="model.items1" multiple>
+          <el-option v-for="item in items" :key="item._id" :label="item.name" :value="item._id"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="逆风出装">
+        <el-select v-model="model.items2" multiple>
+          <el-option v-for="item in items" :key="item._id" :label="item.name" :value="item._id"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="使用技巧">
+        <el-input type="textarea"  placeholder="请输入内容" v-model="model.usageTips"></el-input>
+      </el-form-item>
+      <el-form-item label="对抗技巧">
+        <el-input type="textarea"  placeholder="请输入内容" v-model="model.battleTips"></el-input>
+      </el-form-item>
+      <el-form-item label="团队思路">
+        <el-input type="textarea"  placeholder="请输入内容" v-model="model.teamTips"></el-input>
+      </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
       </el-form-item>
@@ -32,13 +76,24 @@ export default {
     return {
       model: {
         name: '',
-        avatar: ''
-      }
+        avatar: '',
+        title: '',
+        scores: {
+          difficult: 0,
+          skills: 0,
+          attack: 0,
+          survive: 0
+        }
+      },
+      categories: [],
+      items: []
     }
   },
   created() {
     // id为真时，才获取要编辑的内容
     this.id && this.getEditContent()
+    this.getCategories()
+    this.getItems()
   },
   methods: {
     //async await 方式
@@ -61,7 +116,15 @@ export default {
     async getEditContent() {
       const res = await this.$http.get(`rest/heroes/${this.id}`)
       console.log(res.data)
-      this.model = res.data;
+      this.model = Object.assign({}, this.model, res.data)
+    },
+    async getCategories() {
+      const res = await this.$http.get(`rest/categories`)
+      this.categories = res.data
+    },
+    async getItems() {
+      const res = await this.$http.get(`rest/items`)
+      this.items = res.data
     },
     afterUpload(res) {
       console.log(res)
@@ -73,26 +136,26 @@ export default {
 
 <style>
 .avatar-uploader .el-upload {
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
 }
 .avatar-uploader .el-upload:hover {
-    border-color: #409EFF;
+  border-color: #409eff;
 }
 .avatar-uploader-icon {
-    font-size: 28px;
-    color: #8c939d;
-    width: 178px;
-    height: 178px;
-    line-height: 178px !important;
-    text-align: center;
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px !important;
+  text-align: center;
 }
 .avatar {
-    width: 178px;
-    height: 178px;
-    display: block;
+  width: 178px;
+  height: 178px;
+  display: block;
 }
 </style>
