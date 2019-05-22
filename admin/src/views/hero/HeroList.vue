8f8470e87534ed338e4f307917a8e34f1f5f02f4
lwq945 <lwq945@icloud.com>
2019-05-22 09:56:13 +0800

    定义英雄模型所需字段

---
+++ admin/src/views/hero/HeroList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="categoryList">
+    <h1>英雄列表</h1>
+    <el-table stripe :data="items">
+      <el-table-column prop="_id" label="ID" width="240"></el-table-column>
+      <el-table-column prop="name" label="英雄名称"></el-table-column>
+      <el-table-column prop="avatar" label="头像">
+        <template slot-scope="scope">
+          <img :src="scope.row.avatar" style="height: 3rem">
+        </template>
+      </el-table-column>
+      <el-table-column fixed="right" label="操作" width="200">
+        <template slot-scope="scope">
+          <!--  scope.row 是指当前项（操作）的内容对象 {_id: xxx,name: xxxx} -->
+          <el-button
+            type="primary"
+            size="small"
+            @click="$router.push(`/heroes/edit/${scope.row._id}`)"
+          >编辑</el-button>
+          <el-button type="primary" size="small" @click="remove(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: []
+    };
+  },
+  created() {
+    this.getItemLists();
+  },
+  methods: {
+    // 获取物品列表数据
+    async getItemLists() {
+      const res = await this.$http.get('rest/heroes')
+      this.items = res.data
+    },
+    // 删除物品列表选中的数据
+    remove(row) {
+      this.$confirm(`是否确定要删除英雄 "${row.name}"`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        const res = await this.$http.delete(`rest/heroes/${row._id}`)
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        })
+        // 删除后，重新获取列表数据
+        this.getItemLists()
+      })
+    }
+  }
+}
+</script>
--- server/models/Hero.js
+++ server/models/Hero.js
@@ -3,7 +3,30 @@ const schema = mongoose.Schema
 
 const HeroSchema = new schema({
   name: { type: String },
-  avatar: { type: String }
+  avatar: { type: String },
+  title: { type: String },
+  categories: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Category' }],
+  scores: {
+    difficult: { type: Number },
+    skills: { type: Number },
+    attack: { type: Number },
+    survive: { type: Number }
+  },
+  skills: [{
+    icon: { type: String },
+    name: { type: String },
+    description: { type: String },
+    tips: { type: String }
+  }],
+  items1: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Item' }],
+  items2: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Item' }],
+  usageTips: { type: String },
+  battleTips: { type: String },
+  teamTips: { type: String },
+  partners: [{
+    hero: { type: mongoose.SchemaTypes.ObjectId, ref: 'Hero'},
+    description: { type: String }
+  }]
 })
 
 module.exports = mongoose.model('Hero', HeroSchema)
