b1d6706c2d7a0db13ba1375bb13e1fde8eb02bd5
lwq945 <lwq945@icloud.com>
2019-06-03 13:59:27 +0800

    添加web端main页的顶部和导航

--- web/src/router.js
+++ web/src/router.js
@@ -1,25 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+
+import Main from './views/Main.vue'
+import Home from './components/main/Home.vue'
+import Strategy from './components/main/Strategy.vue'
+import Match from './components/main/Match.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      name: 'main',
+      component: Main,
+      children: [
+        { path: '/', name: 'home', component: Home },
+        { path: '/strategy', name: 'strategy', component: Strategy },
+        { path: '/match', name: 'match', component: Match },
+      ]
     }
   ]
 })
