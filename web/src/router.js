da9ec8d2085eae56348a38e1b30bd549b41a0515
lwq945 <lwq945@icloud.com>
2019-06-04 13:50:17 +0800

    reset web端结构

--- web/src/router.js
+++ web/src/router.js
@@ -1,25 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
-import Main from './views/Main.vue'
-import Home from './components/main/Home.vue'
-import Strategy from './components/main/Strategy.vue'
-import Match from './components/main/Match.vue'
+import Home from './views/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'main',
-      component: Main,
-      children: [
-        { path: '/', name: 'home', component: Home },
-        { path: '/strategy', name: 'strategy', component: Strategy },
-        { path: '/match', name: 'match', component: Match },
-      ]
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
