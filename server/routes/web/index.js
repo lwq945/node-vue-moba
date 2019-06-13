6dbf290fa5a05c250be18ddfb80c6991deff73d4
lwq945 <lwq945@icloud.com>
2019-06-13 12:46:41 +0800

    web端接口--获取新闻资讯

--- server/routes/web/index.js
+++ server/routes/web/index.js
@@ -4,12 +4,13 @@ module.exports = app => {
   const mongoose = require('mongoose')
   const Category = mongoose.model('Category')
   const Article = mongoose.model('Article')
-  // const Category = require('../../models/Category')
-  // const Article = require('../../models/Article')
 
+  // 初始化新闻数据
   router.get('/news/init', async (req, res) => {
-    const parent = await Category.findOne({name: '新闻分类'})
-    const cats = await Category.find().where({parent: parent}).lean() //lean()将结果(mongoose Documents)转换成普通js对象返回
+    const parent = await Category.findOne({ name: '新闻分类' })
+    const cats = await Category.find()
+      .where({ parent: parent })
+      .lean() //lean()将结果(mongoose Documents)转换成普通js对象返回
     const newsTitles = [
       '夏日新版本“稷下星之队”即将6月上线',
       '王者荣耀携手两大博物馆 走进稷下学宫',
@@ -33,15 +34,68 @@ module.exports = app => {
       'KRKPL：还在用庄周打辅助？JY边路庄周带你越塔莽！'
     ]
     const newsList = newsTitles.map(title => {
-      const catsRandom = cats.slice(0).sort((a,b) => Math.random() - 0.5)
+      // 根据返回的值(-1,0,1)进行排序
+      const catsRandom = cats.slice(0).sort((a, b) => Math.random() - 0.5)
       return {
-        categories: catsRandom.slice(0,2),
+        categories: catsRandom.slice(0, 2), // 取排序好的前两个
         title: title
       }
-    }) 
+    })
+    // 以任意条件删除Article中的数据
     await Article.deleteMany({})
     await Article.insertMany(newsList)
     res.send(newsList)
+  })
+
+  // 新闻列表
+  router.get('/news/list', async (req, res) => {
+    // 先找出顶级分类
+    const parent = await Category.findOne().where({ name: '新闻分类' })
+    //model.aggregate([]) 使用 聚合 进行关联查询，可以执行多次的条件
+    const cats = await Category.aggregate([
+      { 
+        // 过滤查询：找出字段parent的值为顶级分类_id的所有子分类
+        $match: { 
+          parent: parent._id 
+        } 
+      },
+      {
+        // 关联查询：找出子分类对应的文章标题数据
+        $lookup: {
+          from: 'articles', // 关联集合的名称的小写复数
+          localField: '_id', // 本地键
+          foreignField: 'categories', //外键(关联集合的字段)
+          as: 'newsList' // 输出后的名称
+        }
+      },
+      {
+        // 限制newsList的数据条数
+        $addFields: {
+          newsList: { $slice: ['$newsList', 5] }
+        }
+      }
+    ])
+
+    // 返回所有子分类的_id
+    const subCats = cats.map(v => v._id)
+    // 添加字段 '热门'
+    cats.unshift({
+      name: '热门',
+      newsList: await Article.find().where({
+        categories: { $in: subCats }
+      }).populate('categories').limit(5).lean()
+    })
+
+    //给每个分类的newsList的每一项添加字段 categoryName
+    cats.map(cat => {
+      cat.newsList.map(news => {
+        news.categoryName = (cat.name === '热门') ? news.categories[0].name : cat.name
+        return news
+      })
+      return cat
+    })
+
+    res.send(cats)
   })
 
   app.use('/web/api', router)
